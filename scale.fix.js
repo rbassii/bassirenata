57a0d4730a350e6772ece3beae11850bc8f0a869
Bassi Renata <54715443+rbassii@users.noreply.github.com>
2024-07-18 11:21:37 -0300

    Update scale.fix.js

--- scale.fix.js
+++ scale.fix.js
@@ -1,17 +1,19 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
+document.addEventListener("DOMContentLoaded", function() {
+  const metas = document.getElementsByTagName('meta');
+
+  if (navigator.userAgent.match(/iPhone/i)) {
+    Array.from(metas).forEach(meta => {
+      if (meta.name === "viewport") {
+        meta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+      }
+    });
+
+    document.addEventListener("gesturestart", function() {
+      Array.from(metas).forEach(meta => {
+        if (meta.name === "viewport") {
+          meta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+        }
+      });
+    }, false);
   }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+});
